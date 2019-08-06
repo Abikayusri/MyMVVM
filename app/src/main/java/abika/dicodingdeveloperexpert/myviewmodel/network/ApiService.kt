46cba12d800e8f8d0ce06b5d52f99cb7a0cd9209
Abikayusri <abikayusri96@gmail.com>
2019-08-06 10:41:33 +0700

    New update

--- app/src/main/java/abika/dicodingdeveloperexpert/myviewmodel/network/ApiService.kt
+++ app/src/main/java/abika/dicodingdeveloperexpert/myviewmodel/network/ApiService.kt
@@ -1,11 +1,11 @@
 package abika.dicodingdeveloperexpert.myviewmodel.network
 
 import abika.dicodingdeveloperexpert.myviewmodel.model.Movie
-import android.telecom.Call
 import retrofit2.http.GET
+import retrofit2.Call
 
 interface ApiService {
 
     @GET("upcoming?api_key=c8ff8c510e74a75a3643870242745d71")
-    fun movie():Call<Movie>
+    fun movie(): Call<Movie>
 }
